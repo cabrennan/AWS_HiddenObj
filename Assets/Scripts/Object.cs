e69697be61d176ce6e980480174a5ee899b9cea5
cabrennan <christine@cabrennan.com>
2019-12-30 19:24:30 -0500

    Removed TextMeshPro
    TextMeshPro was causing some buggy behavior, removed it and went back to standard text fields.

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Object", menuName = "Object")]
+public class Object : ScriptableObject
+{
+
+    new public string name = "New Object";    // Name of the item
+    public Sprite icon = null;              // Item icon
+    public bool showInInventory = true;
+    public bool enabled = false; // If true can be clicked on
+    public Vector3 pos;
+
+    public GameObject obj;
+    public Collider2D objCollider;
+    private Camera cam;
+    private Plane[] planes;
+
+
+    public Vector3 center;
+    // Called when the item is pressed in the inventory
+    public virtual void Use()
+    {
+        // Use the item
+
+    }
+
+
+    // Call this method to remove the item from inventory
+    public void RemoveFromInventory()
+    {
+        // Inventory.instance.Remove(this);
+    }
+
+    public void SetPosition()
+    {
+        Debug.Log("Positioning: " + this.name);
+
+       
+    }
+
+}
